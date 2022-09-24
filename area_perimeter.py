c51c2251ea06751c75eb51b1c6b404bb52e6605c
Malik Salem <malik.salem@mths.ca>
2022-09-24 17:18:47 +0000

    code

---
+++ area_perimeter.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+# Created by: maliksalem1
+# Created on: Sep 2022
+# This program calculates the area and perimeter of a circle
+#    with radius 15mm
+
+import math
+
+
+def main():
+    # this function calculates the area and perimeter
+
+    print("If a circle has a radius of 15 mm: ")
+    print("")
+    print("Area is {} mm².".format(math.pi * 15 ** 2))
+    print("Perimeter is {} mm.".format(2 * math.pi * 15))
+    print("")
+    print("Done.")
+
+
+if __name__ == "__main__":
+    main()
