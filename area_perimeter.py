28ee0e100647f5147b54efe965c0f64c907e6679
Malik Salem <malik.salem@mths.ca>
2022-09-25 15:52:41 +0000

    code

--- area_perimeter.py
+++ area_perimeter.py
@@ -12,6 +12,7 @@ def main():
     # this function calculates the area and perimeter
 
     print("If a circle has a radius of 15 mm: ")
+    
     print("Area is {} mm².".format(math.pi * 15 ** 2))
     print("Perimeter is {} mm.".format(2 * math.pi * 15))
     print("\nDone.\n")
